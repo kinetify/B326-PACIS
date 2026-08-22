45a5687f72f3dd374e7572aa9073f103fc9a723c
Joshua <Oshwacis@Oshwacisss.com>
2026-08-22 12:47:29 +0800

    Add code for Arithmetic, Assignment & Relational Operator

---
+++ src/day3/Activity1_PACIS.java
@@ -0,0 +1,25 @@
+public class Activity1_PACIS {
+
+    public static void main(String[] args) {
+
+        boolean myBoolean = true;
+        byte myByte = 100;
+        short myShort = 30000;
+        int myInt = 100000;
+        long myLong = 10000000000L;
+        float myFloat = 3.14f;
+        double myDouble = 3.14159265359;
+        char myChar = 'K';
+        String myString = "Hello, Joysis!";
+
+        System.out.println("Boolean: " + myBoolean);
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + myShort);
+        System.out.println("Int: " + myInt);
+        System.out.println("Long: " + myLong);
+        System.out.println("Float: " + myFloat);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Char: " + myChar);
+        System.out.println("String: " + myString);
+    }
+}
---
+++ src/day3/VariableThursday.java
@@ -0,0 +1,33 @@
+package com.joysistvi.stage1.day3;
+
+public class VariableThursday {
+
+    public static void main(String[] args) {
+
+        String timba = "tubig";
+        int age = 26;
+        String address = "San Andres Cainta Rizal";
+
+        String proLanguage = "C" , proLanguage2a = "Java" , proLanguage3 = "Python";
+
+        System.out.println(proLanguage);
+
+        float temp;
+
+        temp = 37.5f;
+
+        System.out.println("Temperature= " + temp);
+        System.out.println("Age= " + age);
+        System.out.println(address + " " + proLanguage);
+
+
+        String firstName = "Kenneth";
+        String middleInitial = "L";
+        String lastName = "Pacis";
+
+        String fullName = lastName + ", " + firstName + " " + middleInitial + ".";
+
+                System.out.println(fullName);
+
+    }
+}
